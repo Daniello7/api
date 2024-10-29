81117ceea32b07edc0242d528f171c03c3f8f6f0
daniello7 <daniellopezolmos7@gmail.com>
2024-10-29 15:58:19 +0100

    Fin paso 4, Añadido Product: Controller, Resource, cambiado Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Product;
 use App\Models\User;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -15,8 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         Category::factory(10)->create();
+        Product::factory(20)->create();
     }
 }
